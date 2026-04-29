880e95c6313ee2c9c230df9a34b6ecfd9a7eedf5
brufinus <ber.rufinus@gmail.com>
2026-04-29 16:56:54 -0400

    start distribution service

---
+++ services/distribution.py
@@ -0,0 +1,18 @@
+from data.items import ITEMS
+from models.containers.RocketSilo import RocketSilo
+
+
+def distribute_items(num_silos, items):
+    # items = [("item1", 10), ("item2", 5)...]
+    expanded_items = []
+    for item in items:
+        for i in range(0, item[1]):
+            expanded_items.append(ITEMS[item[0]])
+
+    expanded_items.sort(reverse=True, key=lambda d: d['weight'])
+
+    silos = [RocketSilo()]
+    # for silo in silos:
+
+    print(expanded_items)
+    return ""
