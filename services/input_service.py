17ea83025743d8b0362b27845880f76f8c9d5c9b
brufinus <ber.rufinus@gmail.com>
2026-04-29 01:16:38 -0400

    move user input functions to input_service

---
+++ services/input_service.py
@@ -0,0 +1,50 @@
+from data.constants import INPUT_GREATER_ZERO, INPUT_INVALID_NUM
+from data.items import ITEMS
+
+
+def request_silo_count():
+    while True:
+        try:
+            num_silos = int(input("Number of Rocket silos: "))
+            if num_silos > 0:
+                return num_silos
+            else:
+                print(INPUT_GREATER_ZERO)
+        except ValueError:
+            print(INPUT_INVALID_NUM)
+
+
+def request_items():
+    print("Add items to the silo. Enter 'done' once finished.")
+    items = []
+    while True:
+        user_item = input("Item: ")
+        if user_item.lower() == "done":
+            if len(items) == 0:
+                print("At least one item must be added to the silo.")
+            else:
+                break
+        else:
+            item = get_item(user_item)
+            if len(item) > 0:
+                while True:
+                    try:
+                        count = int(input("Count: "))
+                        if count > 0:
+                            items.append((item, count))
+                            break
+                        else:
+                            print(INPUT_GREATER_ZERO)
+                    except ValueError:
+                        print(INPUT_INVALID_NUM)
+            else:
+                print("Invalid item. Enter an item that can be inserted into the rocket silo.")
+
+    return items
+
+
+def get_item(item):
+    transformed_item = item.lower().replace(" ", "_")
+    if transformed_item in ITEMS:
+        return transformed_item
+    return ""
