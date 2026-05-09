81952def49a8022955ecc5b5c9fb475ea8896d05
brufinus <ber.rufinus@gmail.com>
2026-05-08 23:03:41 -0400

    save items into session

--- django-rocket-request/django_distribute/urls.py
+++ django-rocket-request/django_distribute/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "distribute"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("collection/", views.item_collection, name="collection"),
     path("results/", views.results, name="results"),
 ]
--- django-rocket-request/django_distribute/views.py
+++ django-rocket-request/django_distribute/views.py
@@ -1,9 +1,31 @@
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "distribute/index.html", None)
+    """Main page for the application."""
+    request.session.set_test_cookie() 
+
+    items = request.session.get("items", [])
+    request.session["items"] = items
+
+    return render(request, "distribute/index.html")
+
+
+def item_collection(request):
+    if request.method == "POST":
+        if request.session.test_cookie_worked():
+            request.session.delete_test_cookie()
+            print("Cookie test!")
+        else:
+            print("Please enable cookies and try again.")
+
+        item_name = request.POST.get("user-item")
+        items = request.session.get("items", [])
+        items.append(item_name)
+        request.session["items"] = items
+        return JsonResponse({"items": items})
+    return JsonResponse({"items": "Invalid request"})
 
 
 def results(request):
