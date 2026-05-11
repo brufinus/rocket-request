b7b266832d42346d6030ac4cd648b4e46f47bb25
brufinus <ber.rufinus@gmail.com>
2026-05-11 18:27:22 -0400

    extract string constants

--- django-rocket-request/django_distribute/data/constants.py
+++ django-rocket-request/django_distribute/data/constants.py
@@ -1,5 +1,7 @@
 """String constants"""
 
+from enum import StrEnum
+
 # Error messages
 INPUT_GREATER_ZERO = "Enter a value greater than zero."
 INPUT_INVALID_NUM = f"Invalid input. {INPUT_GREATER_ZERO}"
@@ -10,3 +12,10 @@ ITEM_ROCKET_CAPACITY = "rocket_capacity"
 ITEM_WEIGHT = "weight"
 ITEM_ID = "id"
 ITEM_KEYWORDS = "keywords"
+
+
+class Errors(StrEnum):
+    """Error messages."""
+
+    NO_ITEMS_ADDED = "No items have been added."
+    ADD_ITEMS_DISTRIBUTE = "Please add items to distribute."
--- django-rocket-request/django_distribute/views.py
+++ django-rocket-request/django_distribute/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonRespon
 from django.shortcuts import render
 from django.urls import reverse
 
+from django_distribute.data.constants import Errors
 from django_distribute.data.items import ITEMS
 from django_distribute.services.distribution import distribute_items
 from django_distribute.services.initialize_setup import (
@@ -22,7 +23,7 @@ def index(request):
     request.session["itemlist"] = dict(sorted(itemlist.items()))
     table_headers = ("Item", "Count")
     if not itemlist:
-        table_headers = ("No items have been added.", "")
+        table_headers = (Errors.NO_ITEMS_ADDED, "")
 
     distribute_error = request.session.pop("distribute_error", "")
 
@@ -90,7 +91,7 @@ def distributable(request):
     """Checks whether the session is valid for distribution."""
     itemlist: dict = request.session.get("itemlist", {})
     if len(itemlist) <= 0:
-        request.session["distribute_error"] = "Please add items to distribute."
+        request.session["distribute_error"] = Errors.ADD_ITEMS_DISTRIBUTE
         return HttpResponseRedirect(reverse("distribute:index"))
     try:
         num_silos = request.POST["num_silos"]
