0732058b5d28241afdbfb1631f90b86eeb12fe68
brufinus <ber.rufinus@gmail.com>
2026-05-10 22:11:29 -0400

    add django debug toolbar

--- django-rocket-request/rocketrequest/settings.py
+++ django-rocket-request/rocketrequest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "debug_toolbar",
     "django_distribute.apps.DistributeConfig",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -116,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+INTERNAL_IPS = ["127.0.0.1"]
