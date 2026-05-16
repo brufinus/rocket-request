6e4264eec5bf414169f9a3ee98cc8d57e54d5493
brufinus <ber.rufinus@gmail.com>
2026-05-15 20:30:40 -0400

    uninstall admin app

--- django-rocket-request/rocketrequest/settings.py
+++ django-rocket-request/rocketrequest/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "django_distribute.apps.DistributeConfig",
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
