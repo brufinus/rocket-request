6593730ab04c644a30fe8dad6e5a330f418526b6
brufinus <ber.rufinus@gmail.com>
2026-05-20 18:49:20 -0400

    feat(api): add module for generating blueprint strings

---
+++ django-distribute/django_distribute/services/blueprint.py
@@ -0,0 +1,90 @@
+"""Functions to generate blueprint strings from JSON representations."""
+
+import base64
+import json
+import zlib
+
+
+def generate_bp_from_json(json_obj: object) -> str:
+    """
+    Generates a blueprint string from a JSON object representation.
+
+    The JSON is compressed then encoded using
+    base64 with a version byte at the front.
+
+    :param object json_obj: JSON representation of the blueprint.
+    :return: The blueprint string.
+    :rtype: str
+    """
+    json_str = json.dumps(json_obj).replace(" ", "")
+    json_bytes = json_str.encode("utf-8")
+    json_compressed = zlib.compress(json_bytes, 9)
+    return (b"0" + base64.b64encode(json_compressed)).decode("utf-8")
+
+
+def generate_book(chests: list) -> object:
+    """
+    Generates a JSON representation of a blueprint book.
+
+    :param list chests: List of JSON-represented requester chests.
+    :returns: JSON-represented blueprint book.
+    :rtype: object
+    """
+    return {
+        "blueprint_book": {
+            "blueprints": chests,
+            "item": "blueprint-book",
+            "active_index": 0,
+            "version": 562949958205441,
+        }
+    }
+
+
+def generate_chest(silo_num: int, items: list) -> object:
+    """
+    Generates a JSON representation of a requester chest.
+
+    :param int silo_num: The silo that the chest is assigned to.
+    :param list items: List of JSON-represented items.
+    :return: JSON-represented requester chest.
+    :rtype: object
+    """
+    return {
+        "blueprint": {
+            "icons": [{"signal": {"name": "requester-chest"}, "index": 1}],
+            "entities": [
+                {
+                    "entity_number": 1,
+                    "name": "requester-chest",
+                    "position": {"x": -95.5, "y": -198.5},
+                    "request_filters": {
+                        "sections": [{"index": 1, "filters": items}],
+                        "trash_not_requested": True,
+                    },
+                }
+            ],
+            "item": "blueprint",
+            "label": str(silo_num),
+            "version": 562949958205441,
+        },
+        "index": silo_num - 1,
+    }
+
+
+def generate_item(index: int, name: str, count: int) -> object:
+    """
+    Generates a JSON representation of an item.
+
+    :param int index: 1-based request slot.
+    :param str name: The item name in slug format.
+    :param int count: The item count.
+    :return: JSON-represented item.
+    :rtype: object
+    """
+    return {
+        "index": index,
+        "name": name,
+        "quality": "normal",
+        "comparator": "=",
+        "count": count,
+    }
