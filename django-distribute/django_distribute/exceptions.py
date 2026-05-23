a2d3b92b46bac3f39a2c4047d275baba933693a9
brufinus <ber.rufinus@gmail.com>
2026-05-23 00:32:08 -0400

    feat(exceptions): add exceptions module

---
+++ django-distribute/django_distribute/exceptions.py
@@ -0,0 +1,20 @@
+"""Defines custom exceptions used by distribute."""
+
+
+class InvalidBlueprintException(Exception):
+    """Exception raised when an imported blueprint is invalid."""
+
+    def __init__(self, message) -> None:
+        super().__init__(message)
+
+
+class InvalidItemException(Exception):
+    """Exception raised when importing an invalid item."""
+
+    def __init__(self, item) -> None:
+        self.message = "Invalid item imported"
+        self.item = item
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return f"{self.message}: {self.item}"
