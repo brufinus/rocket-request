c08617c8d63e12bd4d714191d19f98a66712878c
brufinus <ber.rufinus@gmail.com>
2026-05-23 23:36:38 -0400

    chore: enable parallel testing
    chore: settings docstring

---
+++ django-distribute/test_settings.py
@@ -0,0 +1,43 @@
+"""Settings for the test runner."""
+
+SECRET_KEY = ("django-insecure-$4gbgz8ea%peul=b!2d3oj3ln3j(ar$t1uccym+p@jhp))rt_8",)
+
+INSTALLED_APPS = [
+    "django_distribute",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "django_distribute.tests.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+
+STATIC_URL = "static/"
